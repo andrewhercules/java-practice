c2c00aa77cd9c5251b007d946b86491de00175a1
Andrew Hercules <andrewhercules@gmail.com>
2015-01-13 16:42:53 -0500

    IfDemo.java - brief introduction to if statements in Java

---
+++ chapter-1/IfDemo.java
@@ -0,0 +1,19 @@
+class IfDemo {
+  public static void main(String args[]) {
+    int a, b, c;
+    a = 2;
+    b = 3;
+    if(a < b) System.out.println("a is less than b");
+    if(a == b) System.out.println("a is equal to b, but you won't see this");
+    System.out.println();
+    c = a - b;
+    System.out.println("c contains " + c);
+    if(c >= 0) System.out.println("c is not a negative number");
+    if(c < 0) System.out.println("c is a negative number");
+    System.out.println();
+    c = b - a;
+    System.out.println("c contains " + c);
+    if(c >= 0) System.out.println("c is not a negative number");
+    if(c < 0) System.out.println("c is a negative number");
+  }
+}
