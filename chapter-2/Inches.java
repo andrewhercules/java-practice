0ef9781231e053383ddcdc2919c1fabff1de3ab5
Andrew Hercules <andrewhercules@gmail.com>
2015-01-14 09:42:23 -0500

    Inches.java - compute cubic inches in cubic mile to see difference between long and int types

---
+++ chapter-2/Inches.java
@@ -0,0 +1,8 @@
+class Inches {
+  public static void main(String args[]) {
+    long ci, im;
+    im = 5280 * 12;
+    ci = im * im * im;
+    System.out.println("There are " + ci + " cubic inches in a cubic mile.");
+  }
+}
