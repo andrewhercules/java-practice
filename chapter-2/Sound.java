0db6a8a4f62df617a33c2a42ecb390fe75b38c4f
Andrew Hercules <andrewhercules@gmail.com>
2015-01-14 10:22:53 -0500

    Sound.java - calculate distance to lightning

---
+++ chapter-2/Sound.java
@@ -0,0 +1,9 @@
+class Sound {
+  public static void main(String args []) {
+    double dist;
+    double echoTime;
+    echoTime = 15;
+    dist = (echoTime / 2) * 1100;
+    System.out.println("The lightning is " + dist + " feet away.");
+  }
+}
