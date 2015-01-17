3ff1a3fc2c1ea12c43339786358d438e55e4dfe2
Andrew Hercules <andrewhercules@gmail.com>
2015-01-17 17:02:29 -0500

    Updated Help.java app with information on for, while, and do-while loops

--- chapter-3/Help.java
+++ chapter-3/Help.java
@@ -1,12 +1,23 @@
 class Help {
   public static void main(String args [])
     throws java.io.IOException {
-      char choice;
-      System.out.println("Help on:");
-      System.out.println(" 1. if");
-      System.out.println(" 2. switch");
-      System.out.print("Choose one: ");
-      choice = (char) System.in.read();
+      char choice, ignore;
+
+      do {
+        System.out.println("Help on:");
+        System.out.println(" 1. if");
+        System.out.println(" 2. switch");
+        System.out.println(" 3. for");
+        System.out.println(" 4. while");
+        System.out.println(" 5. do-while");
+        System.out.print("Choose one: ");
+        choice = (char) System.in.read();
+        do {
+          ignore = (char) System.in.read();
+        } while (ignore != '\n');
+
+      } while (choice < '1' | choice > '5');
+
       switch(choice) {
         case '1':
           System.out.println("The if statement: \n");
@@ -22,8 +33,21 @@ class Help {
           System.out.println(" // ...");
           System.out.println("}");
           break;
-        default:
-          System.out.println("Selection not found!");
+        case '3':
+          System.out.println("The for:\n");
+          System.out.print("for(init; condition; iteration)");
+          System.out.println(" statement;");
+          break;
+        case '4':
+          System.out.println("The while:\n");
+          System.out.println("while(condition) statement;");
+          break;
+        case '5':
+          System.out.println("The do-while:\n");
+          System.out.println("do {");
+          System.out.println("  statement;");
+          System.out.println("} while (condition);");
+          break;
         }
     }
 }
