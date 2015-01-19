e2755d71af8029c73d4d0a8d4224fc3772eb7e09
Andrew Hercules <andrewhercules@gmail.com>
2015-01-18 19:20:21 -0500

    Updated Help.java with info on break and continue statements and infinite for loop that requires user press q to quit

--- chapter-3/Help.java
+++ chapter-3/Help.java
@@ -3,6 +3,7 @@ class Help {
     throws java.io.IOException {
       char choice, ignore;
 
+      for(;;) {
       do {
         System.out.println("Help on:");
         System.out.println(" 1. if");
@@ -10,13 +11,18 @@ class Help {
         System.out.println(" 3. for");
         System.out.println(" 4. while");
         System.out.println(" 5. do-while");
-        System.out.print("Choose one: ");
+        System.out.println(" 6. break");
+        System.out.println(" 7. continue\n");
+        System.out.print("Choose one (or press 'q' to quit): ");
         choice = (char) System.in.read();
         do {
           ignore = (char) System.in.read();
         } while (ignore != '\n');
 
-      } while (choice < '1' | choice > '5');
+      } while (choice < '1' | choice > '7' & choice != 'q');
+
+      if (choice == 'q') break;
+      System.out.println("\n");
 
       switch(choice) {
         case '1':
@@ -48,6 +54,16 @@ class Help {
           System.out.println("  statement;");
           System.out.println("} while (condition);");
           break;
+        case '6':
+          System.out.println("The break:\n");
+          System.out.println("break; or break label;");
+          break;
+        case '7':
+          System.out.println("The continue:\n");
+          System.out.println("continue; or continue label;");
+          break;
         }
+        System.out.println();
+      }
     }
 }
